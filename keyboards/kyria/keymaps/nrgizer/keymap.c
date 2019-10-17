8fb9a68d7c691ce970eefdf0ff34bef4fab451bf
Carl-Magnus Björkell <cmb@tradeshift.com>
2019-10-17 13:10:38 +0200

    Add some emoji awesomeness
    Add a thumbsup and laughing face for the two keys that I won't be using
    much otherwize. This seem so work everywhere but whatsapp for now that
    just does three of them for some reason.

--- keyboards/kyria/keymaps/nrgizer/keymap.c
+++ keyboards/kyria/keymaps/nrgizer/keymap.c
@@ -28,7 +28,9 @@
 #define _I3MOVE 10
 
 enum keycodes {
-  DYNAMIC_MACRO_RANGE = SAFE_RANGE
+  THUMBSUP = SAFE_RANGE,
+  LAUGHING,
+  DYNAMIC_MACRO_RANGE
 };
 
 #include "dynamic_macro.h"
@@ -51,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK] = LAYOUT(
       LCTL(LGUI(KC_SPACE)),KC_Q,         KC_W,         LT(_I3WORK,KC_F), LT(_I3MOVE,KC_P), KC_G,                                         KC_J, KC_L,         KC_U,         KC_Y,         KC_SCLN,      _______, \
       KC_TAB,              LSFT_T(KC_A), LCTL_T(KC_R), LALT_T(KC_S),     LT(_NAVS,KC_T),   LT(_FUNC,KC_D),                               KC_H, LGUI_T(KC_N), LALT_T(KC_E), LCTL_T(KC_I), LSFT_T(KC_O), KC_QUOT, \
-      KC_LSFT,             KC_Z,         KC_X,         KC_C,             LT(_NUM,KC_V),    KC_B,   XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, KC_K, KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,      KC_RSFT, \
+      KC_LSFT,             KC_Z,         KC_X,         KC_C,             LT(_NUM,KC_V),    KC_B,   THUMBSUP, XXXXXXX, XXXXXXX, LAUGHING, KC_K, KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,      KC_RSFT, \
               XXXXXXX, XXXXXXX, KC_DEL, LCTL_T(KC_SPC),LGUI_T(KC_ENT), KC_LEAD, KC_BSPACE, MO(_SYMB), LT(_SHORT,KC_ESCAPE), XXXXXXX
 
     ),
@@ -292,6 +294,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (!process_record_dynamic_macro(keycode, record)) {
         return false;
+    }
+
+    if (record->event.pressed) {
+        switch(keycode) {
+            case THUMBSUP:
+                SEND_STRING(SS_LCTRL(SS_LSFT("u")) "1f44d" SS_TAP(X_ENTER));
+                return false;
+            case LAUGHING:
+                SEND_STRING(SS_LCTRL(SS_LSFT("u")) "1f606" SS_TAP(X_ENTER));
+                return false;
+        }
     }
     return true;
 }
